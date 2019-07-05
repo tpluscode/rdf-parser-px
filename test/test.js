2af1675046e6851f944d2d79e4aa9cbd561a525d
bergi <bergi@axolotlfarm.org>
2019-07-05 15:37:21 +0200

    added support for decimals

--- test/test.js
+++ test/test.js
@@ -42,4 +42,17 @@ describe('rdf-parser-px', () => {
 
     expect(ntriples).toBe(expected)
   })
+
+  test('converts the decimals example to double values', async () => {
+    const expected = fs.readFileSync('test/support/decimals2.px.nt').toString().trim()
+    const pxStream = fs.createReadStream('test/support/decimals2.px')
+
+    const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/' })
+    const quadStream = parser.import(pxStream)
+
+    const quads = await streamConcat(quadStream)
+    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
+
+    expect(ntriples).toBe(expected)
+  })
 })
