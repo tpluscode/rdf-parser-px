de573db717321cc33698b8b978534bff28cb9399
bergi <bergi@axolotlfarm.org>
2019-07-05 15:39:50 +0200

    use metadata in examples

--- examples/anzahl-forstbetriebe-ch.js
+++ examples/anzahl-forstbetriebe-ch.js
@@ -5,7 +5,11 @@ async function main () {
   const url = 'https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file=px-x-0703010000_103'
   const pxStream = (await fetch(url)).body
 
-  const parser = new RdfPxParser({ baseIRI: 'http://example.org/anzahl-forstbetriebe/' })
+  const columns = [{
+    titles: 'Jahr',
+    datatype: 'http://www.w3.org/2001/XMLSchema#gYear'
+  }]
+  const parser = new RdfPxParser({ baseIRI: 'http://example.org/anzahl-forstbetriebe/', columns })
   const quadStream = parser.import(pxStream)
 
   quadStream.on('data', quad => {
--- examples/pflanzungen-ch.js
+++ examples/pflanzungen-ch.js
@@ -5,7 +5,11 @@ async function main () {
   const url = 'https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file=px-x-0703030000_112'
   const pxStream = (await fetch(url)).body
 
-  const parser = new RdfPxParser({ baseIRI: 'http://example.org/pflanzungen/' })
+  const columns = [{
+    titles: 'Jahr',
+    datatype: 'http://www.w3.org/2001/XMLSchema#gYear'
+  }]
+  const parser = new RdfPxParser({ baseIRI: 'http://example.org/pflanzungen/', columns })
   const quadStream = parser.import(pxStream)
 
   quadStream.on('data', quad => {
