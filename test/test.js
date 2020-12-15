9b27ca2394c592ab312e9b7f98ad8c90fe71a0c0
bergi <bergi@axolotlfarm.org>
2020-12-15 23:01:31 +0100

    use new cube model + small cleanup

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@
 const fs = require('fs')
 const namespace = require('@rdfjs/namespace')
 const getStream = require('get-stream')
+const rdf = require('rdf-ext')
+const fromFile = require('rdf-utils-fs/fromFile.js')
 const RdfPxParser = require('..')
 
 const ns = {
@@ -15,33 +17,27 @@ describe('rdf-parser-px', () => {
   })
 
   test('converts the simple example to RDF', async () => {
-    const expected = fs.readFileSync('test/support/simple.px.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/simple.px.ttl'))
     const pxStream = fs.createReadStream('test/support/simple.px')
 
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/' })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('converts the heading example to RDF and spreads the values', async () => {
-    const expected = fs.readFileSync('test/support/heading.px.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/heading.px.ttl'))
     const pxStream = fs.createReadStream('test/support/heading.px')
 
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/' })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('converts the simple example to RDF using the given column definition', async () => {
-    const expected = fs.readFileSync('test/support/simple.px.columns.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/simple.px.columns.ttl'))
     const pxStream = fs.createReadStream('test/support/simple.px')
 
     const columns = [{
@@ -51,52 +47,41 @@ describe('rdf-parser-px', () => {
       titles: 'Wert',
       datatype: ns.xsd.double.value
     }]
+
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/', columns })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('converts the decimals example to double values', async () => {
-    const expected = fs.readFileSync('test/support/decimals2.px.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/decimals2.px.ttl'))
     const pxStream = fs.createReadStream('test/support/decimals2.px')
 
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/' })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('converts the codepage example using the given codepage', async () => {
-    const expected = fs.readFileSync('test/support/codepage.px.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/codepage.px.ttl'))
     const pxStream = fs.createReadStream('test/support/codepage-not-defined.px')
 
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/', encoding: 'iso-8859-15' })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('converts the codepage example and reads the codepage from the pairs', async () => {
-    const expected = fs.readFileSync('test/support/codepage.px.nt').toString().trim()
+    const expected = await rdf.dataset().import(fromFile('test/support/codepage.px.ttl'))
     const pxStream = fs.createReadStream('test/support/codepage.px')
 
     const parser = new RdfPxParser({ baseIRI: 'http://example.org/simple/' })
-    const quadStream = parser.import(pxStream)
+    const dataset = await rdf.dataset().import(parser.import(pxStream))
 
-    const quads = await getStream.array(quadStream)
-    const ntriples = quads.map(quad => quad.toString()).join('\n').trim()
-
-    expect(ntriples).toBe(expected)
+    expect(dataset.toCanonical()).toBe(expected.toCanonical())
   })
 
   test('throws an error when trying to convert the codepage example with a given encoding different to the one in the px files', async () => {
